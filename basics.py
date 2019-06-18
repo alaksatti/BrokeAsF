c25b1fa34b3331edef16b8378147ab013d634b11
alaksatti <alaksatti@gmail.com>
2019-06-18 00:00:00 +0000

    update dict

--- basics.py
+++ basics.py
@@ -185,7 +185,7 @@ def singleline_int():
 
 def extract(dict):
     ''' extract single key value pairs '''
-    print(dict.get('color'))
+    print(dict.items())
 
 
 extract({'color': 'red', 'fruit': 'apple', 'ripe': 'soon'})
