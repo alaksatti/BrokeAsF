1bff918f12f06f68e3b69fb6ce85572b7da1ef43
alaksatti <alaksatti@gmail.com>
2020-04-14 20:58:35 +0000

    initial commit of gcd function

--- basics.py
+++ basics.py
@@ -10,3 +10,9 @@ def inlist_1(list_1, list_2):
     ''' returns items in list 1 that are not in list 2 '''
     return set(list_1) - set(list_2)
 
+def gcd(a, b):
+    ''' returns the greatest common denominator '''
+    if b:
+        return gcd(b, a % b)
+    else:
+        return a
