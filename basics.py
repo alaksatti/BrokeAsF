ba3c3adae658f6428d0d525c220c3da032c7e762
alaksatti <alaksatti@gmail.com>
2020-04-14 20:55:03 +0000

    initial commit of concatenate

---
+++ basics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def concatenate(list):
+    ''' concatenate all elements in a list into a string and return it '''
+    concatenated = ''.join(map(str, list))
+    return concatenated
+
