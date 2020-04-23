28131a4fa96727c08071c0d329a4e1fe0172b861
alaksatti <ala.k.satti@gmail.com>
2020-04-23 03:34:10 +0000

    updated

--- basic_algos2.py
+++ basic_algos2.py
@@ -22,3 +22,19 @@ def rm_third(list, index=0):
 
 
 rm_third([10, 20, 30, 40, 50, 60, 70, 80, 90])
+lists = [1, -6, 4, 2, -1, 2, 0, -2, 0]
+
+idx1, idx2, idx3 = 0, 1, 2
+
+while idx3 < len(lists):
+    l = [lists[idx1], lists[idx2], lists[idx3]]
+
+    u = [-x if x < 0 else x for x in l]
+
+    if len(set(u)) == 3 and sum(l) == 0:
+        print(l)
+
+
+    idx1+=1
+    idx2+=1
+    idx3+=1
