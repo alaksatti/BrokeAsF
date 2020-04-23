dec95c254225b30c7c2aa0db69b425f60173ce8f
alaksatti <alaksatti@gmail.com>
2020-04-23 01:58:29 +0000

    update remove every third number

--- basic_algos2.py
+++ basic_algos2.py
@@ -6,8 +6,19 @@ def dif(list):
 
 def rm_third(list, index=0):
     ''' remove every third number from a list of numbers '''
+    print(list)
+    
     l = len(list)
+
+    if l == 0:
+        return
 
     index = (index + 2) % l
 
-    
+    list.remove(list[index])
+    rm_third(list)
+
+
+
+
+rm_third([10, 20, 30, 40, 50, 60, 70, 80, 90])
