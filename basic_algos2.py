1033355169112528d360c63d9ca7f79a0fbcf311
alaksatti <alaksatti@gmail.com>
2020-04-23 01:52:18 +0000

    update remove every third number

--- basic_algos2.py
+++ basic_algos2.py
@@ -4,3 +4,10 @@ def dif(list):
     ''' returns True if all numbers in list are different otherwise False '''
     return len(list) == len(set(list))
 
+def rm_third(list, index=0):
+    ''' remove every third number from a list of numbers '''
+    l = len(list)
+
+    index = (index + 2) % l
+
+    
